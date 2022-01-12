a440553d681470cc60dbce9ee1a22708fc9e8c61
kirtigupta2458 <72331388+kirtigupta2458@users.noreply.github.com>
2022-01-12 17:09:07 +0530

    Update CursorEvents.js

--- js/CursorEvents.js
+++ js/CursorEvents.js
@@ -3,33 +3,11 @@ AFRAME.registerComponent("cursor-listener", {
     selectedItemId: { default: "", type: "string" }
   },
   init: function () {
-    this.handleClickEvents();
+    
     this.handleMouseEnterEvents();
     this.handleMouseLeaveEvents();
   },
-  handleClickEvents: function () {
-    //Cursor 'click' Events
-    this.el.addEventListener("click", evt => {
-      const placesContainer = document.querySelector("#places-container");
-      const { state } = placesContainer.getAttribute("tour");
-
-      if (state === "places-list") {
-        const id = this.el.getAttribute("id");
-        const placesId = [
-          "taj-mahal",
-          "budapest",
-          "new-york-city",
-          "eiffel-tower"
-        ];
-        if (placesId.includes(id)) {
-          placesContainer.setAttribute("tour", {
-            state: "view",
-            selectedCard: id
-          });
-        }
-      }
-    });
-  },
+  
   handlePlacesListState: function () {
     const id = this.el.getAttribute("id");
     const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
